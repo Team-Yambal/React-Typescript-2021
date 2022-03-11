023f30696987ce98afa4b8bd72f3d4dc1bb36f6d
yambal <jy@yambal.net>
2022-03-11 23:31:44 +0900

    add React Hoock Form

--- src/features/samples/Samples.tsx
+++ src/features/samples/Samples.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router'
 import { Link } from 'react-router-dom'
 import { CounterContainer } from './counter/CounterContainer'
 import NewsContainer from './news/NewsContainer'
+import { SampleForm } from './sampleForm/SampleForm'
 import WeatherContainer from './weather/WeatherContainer'
 
 export const Samples = () => {
@@ -11,12 +12,14 @@ export const Samples = () => {
       <Link to="/sample/counter">Counter</Link>
       <Link to="/sample/weather">Weather</Link>
       <Link to="/sample/news">News</Link>
+      <Link to="/sample/form">Form</Link>
       <Switch>
         <Route exact path={'/sample/counter'}>
           <CounterContainer />
         </Route>
         <Route exact path={'/sample/weather'} children={<WeatherContainer />} />
         <Route exact path={'/sample/news'} children={<NewsContainer />} />
+        <Route exact path={'/sample/form'} children={<SampleForm />} />
         <Route>Notfound</Route>
       </Switch>
     </>
